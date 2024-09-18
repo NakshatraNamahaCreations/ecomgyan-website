510a57bde04e71df675ba96540da3efe5e4a977b
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2024-09-18 14:47:15 +0530

    changes
    changes

--- src/Components/Privacy.js
+++ src/Components/Privacy.js
@@ -4,90 +4,118 @@ function Privacy() {
   return (
     <div className="container">
       <div className="row">
-        <div className="poppins-black mt-3">Privacy Policy</div>
+        <div className="poppins-black mt-3">1. Introduction</div>
         <p className="poppins-regular mt-2">
-          This Privacy Policy governs the manner in which the School collects,
-          uses, maintains and discloses information collected from users (each,
-          a “Student”) of the School. This Privacy Policy applies to the School
-          and all Courses offered by the School.
+          Proleverage.io ("we", "us", "our") operates the Proleverage.io
+          platform, including our website and mobile applications (collectively,
+          the "Services"). This Privacy Policy outlines how we collect, use,
+          share, and protect your personal information when you use our
+          Services, with a specific focus on users located in India and students
+          accessing our courses.
         </p>
 
-        <div className="poppins-medium">
-          Personal identification information
+        <div className="poppins-medium">2. Information We Collect</div>
+        <div className="poppins-regular">
+          Personal Information: "Personal Information" refers to information
+          that can identify an individual, such as: Contact Information: Name,
+          email address, phone number, postal address. Account Information:
+          Username, password, and details provided for creating an account and
+          logging in. Course Information: Data related to course access,
+          progress, and feedback. Usage Data: Information collected
+          automatically when you use our Services, including: IP address, device
+          identifiers, browser type, operating system. Pages visited, duration
+          of visits, interaction with content.
+        </div>
+
+        <div className="poppins-medium pt-2">3. How We Collect Informatio</div>
+        <div className="poppins-regular">
+          Personal Information: We collect this information directly from you
+          when you register for an account, purchase courses, or communicate
+          with us. Usage Data: Collected automatically through cookies and
+          similar technologies during your use of our Services.
+        </div>
+
+        <div className="poppins-medium pt-2">
+          4. How We Use Your Information
         </div>
         <div className="poppins-regular">
-          We may collect personal identification information from Students in a
-          variety of ways, including, but not limited to, when Students enroll
-          in the School or a Course within the School, subscribe to a
-          newsletter, and in connection with other activities, services,
-          features, or resources we make available in our School. Students may
-          visit the School anonymously. We will collect personal identification
-          information from Students only if they voluntarily submit such
-          information to us. Students can refuse to supply personal
-          identification information but doing so may prevent them from engaging
-          in certain School related activities.
+          We use your information to: Provide, operate, and improve our
+          Services. Facilitate course access and track your progress.
+          Communicate with you, including sending updates and promotional
+          materials related to our Services. Ensure the security and integrity
+          of our Services.
         </div>
-        <div className="poppins-regular">How we use collected information</div>
-        <div className="">
-          <div className="poppins-regular mt-1">
-            The School may collect and use Students’ personal identification
-            information for the following purposes:
-          </div>
-          <ul className="pt-2">
-            <li className="poppins-regular">
-              To improve customer service Information you provide helps us
-              respond to your customer service requests and support needs more
-              efficiently.
-            </li>
-            <li className="poppins-regular">
-              To personalize user experience We may use information in the
-              aggregate to understand how our Students as a group use the
-              services and resources provided in our School.
-            </li>
-            <li className="poppins-regular">
-              To send periodic emails We may use Student email addresses to send
-              Students information and updates pertaining to their order.
-              Student email addresses may also be used to respond to Student
-              inquiries, questions, or other requests.
-            </li>
-          </ul>
-        </div>
-        <div className="poppins-medium">Sharing your personal information</div>
-        <div className="poppins-regular pt-1">
-          We do not sell, trade, or rent Student personal identification
-          information to others.
-        </div>
-        <div className="poppins-regular">Third party websites</div>
+
+        <div className="poppins-medium pt-2">5. Sharing and Disclosure</div>
         <div className="poppins-regular">
-          Student may find advertising or other content in our School that link
-          to the websites and services of our partners, suppliers, advertisers,
-          sponsors, licensors and other third parties. We do not control the
-          content or links that appear on these websites and are not responsible
-          for the practices employed by websites linked to or from our School.
-          In addition, these websites or services, including their content and
-          links, may be constantly changing. These websites and services may
-          have their own privacy policies and customer service policies.
-          Browsing and interaction on any other website, including websites
-          which have a link to our Student, is subject to that website's own
-          terms and policies.
+          We may share your information in the following circumstances: Service
+          Providers: With third-party service providers who assist us in
+          operating our Services. Legal Requirements: When required by law or to
+          protect our rights. Business Transfers: In the event of a merger,
+          acquisition, or other business transactions. We do not sell or rent
+          your personal information to third parties.
         </div>
+
+        <div className="poppins-medium pt-2">6. Data Security</div>
+        <div className="poppins-regular">
+          We implement reasonable security measures to protect your information
+          from unauthorized access or disclosure. However, no system is
+          completely secure, and we cannot guarantee absolute security.
+        </div>
+
+        <div className="poppins-medium pt-2">7. Children's Privacy</div>
+        <div className="poppins-regular">
+          Our Services are not intended for children under the age of 13. We do
+          not knowingly collect personal information from children under 13. If
+          we become aware that we have collected such information, we will take
+          steps to delete it. For users aged 13 and above, we encourage parents
+          to be involved in their children's online activities and to monitor
+          their interactions with our Services.
+        </div>
+
+        <div className="poppins-medium pt-2">8. Course Access and Privacy</div>
+        <div className="poppins-regular">
+          For students accessing courses through our website or app: Account
+          Creation: Students must create an account to access courses. Course
+          Data: We collect data related to course enrollment, progress, and
+          feedback. Course Materials: Students may download course materials for
+          offline access. These materials are subject to our Terms & Conditions
+          and should not be shared without permission.
+        </div>
+
+        <div className="poppins-medium pt-2">9. Your Choices</div>
+        <div className="poppins-regular">
+          You have the right to: Access and update your personal information
+          through your account settings. Opt out of promotional communications
+          by following the unsubscribe instructions provided in those
+          communications.
+        </div>
+
+        <div className="poppins-medium pt-2">
+          10. Changes to This Privacy Policy
+        </div>
+        <div className="poppins-regular">
+          You have the right to: Access and update your personal information
+          through your account settings. Opt out of promotional communications
+          by following the unsubscribe instructions provided in those
+          communications.
+        </div>
+
+        <div className="poppins-medium pt-2">11. Contact Us</div>
+        <div className="poppins-regular">
+          If you have any questions or concerns about this Privacy Policy or our
+          data practices, please contact us at:
+        </div>
+
         <div className="poppins-medium mt-2">
-          Changes to this Privacy Policy
+          Email: <span className="poppins-regular">support@proleverage.io</span>
         </div>
-        <div className="poppins-regular pt-1">
-          The School has the discretion to update this Privacy Policy at any
-          time. We encourage Students to frequently check this page for any
-          changes. You acknowledge and agree that it is your responsibility to
-          review this Privacy Policy periodically and become aware of
-          modifications.
-        </div>
-        <div className="poppins-regular">Your acceptance of these terms</div>
-        <div className="poppins-regular mb-3">
-          By enrolling in the School, you signify your acceptance of this
-          Privacy Policy. If you do not agree to this Privacy Policy, please do
-          not enroll in the School. Your continued enrollment in the School
-          following the posting of changes to this Privacy Policy will be deemed
-          your acceptance of those changes.
+        <div className="poppins-medium mt-2 mb-3">
+          Address:{" "}
+          <span className="poppins-regular ">
+            456C, Sri Sai Heights, 3rd floor, Ideal Homes, Rajarajeshwari Nagar,
+            Bangalore 560098
+          </span>
         </div>
       </div>
     </div>
